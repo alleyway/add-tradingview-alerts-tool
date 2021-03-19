0c699e2b2fa504dfdbdbb651b902a9d54b023062
mlake <mlake@alleywayapps.com>
2021-03-19 15:54:58 +0100

    setting log lev variable different

--- src/service/log.ts
+++ src/service/log.ts
@@ -3,8 +3,8 @@ import kleur from "kleur";
 
 const DEFAULT_LEVEL = 3
 
-const log = consola.create({level: Number(process.env.LOG_LEVEL) || DEFAULT_LEVEL})
+const log = consola.create({level: Number(process.env.ATAT_LOG_LEVEL) || DEFAULT_LEVEL})
 
-log.info(`Current Log level: ${kleur.yellow(log.level)} (you can set env variable LOG_LEVEL=5 for verbose logging)`)
+log.info(`Current Log level: ${kleur.yellow(log.level)} (you can set env variable ATAT_LOG_LEVEL=5 for verbose logging)`)
 
 export default log;

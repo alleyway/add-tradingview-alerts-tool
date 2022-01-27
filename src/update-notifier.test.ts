3c1aea1205c39f842e949eb8a12587bcbb3bf084
mlake <mlake@alleywayapps.com>
2022-01-27 12:13:37 +0100

    work on getting ci builds properly updated

--- src/update-notifier.test.ts
+++ src/update-notifier.test.ts
@@ -3,8 +3,15 @@ import {updateNotifier} from "./update-notifier";
 
 describe('Update Notifier Tests', () => {
 
-    it('updateNotifier()', async () => {
+    it('updateNotifier() - latest', async () => {
         const message = await updateNotifier("1.9.3")
+        console.log(message)
+        expect(message).toContain("version of atat available");
+    });
+
+    it('updateNotifier() - beta', async () => {
+        const message = await updateNotifier("1.9.5-beta.0")
+        console.log(message)
         expect(message).toContain("version of atat available");
     });
 

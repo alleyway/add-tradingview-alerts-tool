d7539ec539d67f0b1eec40ce749887f210682e16
Michael Lake <mlake@alleywayapps.com>
2023-10-12 03:30:20 +0200

    update deps slight change for module resulution to satisfy typescript

--- dist/service/log.d.ts
+++ dist/service/log.d.ts
@@ -1,3 +1,3 @@
-declare const log: import("consola/core").ConsolaInstance;
+declare const log: import("consola/dist/core").ConsolaInstance;
 export declare const logLogInfo: () => void;
 export default log;

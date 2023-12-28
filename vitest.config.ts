42d392422966924e3249759620f05fd672595b7b
Michael Lake <mlake@alleywayapps.com>
2023-12-28 16:22:50 +0100

    output unit tests again

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,14 @@ export default defineConfig({
     test: {
         globals: true,
         setupFiles: ['dotenv/config'],
+        outputFile: {
+            junit: "junit.xml",
+            html: "./coverage/index.html"
+        },
+        reporters: ["html", "junit"],
         coverage: {
             provider: 'v8', // or 'istanbul'
-            reporter: "cobertura",
+            reporter: ["cobertura"],
             reportsDirectory: "./coverage"
         },
     },

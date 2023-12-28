ad9f28887ab075a90f2f114599642fbfcfb2118b
Michael Lake <mlake@alleywayapps.com>
2023-12-28 14:40:05 +0100

    load dotenv

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         globals: true,
+        setupFiles: ['dotenv/config'],
         coverage: {
             provider: 'v8', // or 'istanbul'
             reporter: ['text', 'json', 'html'],

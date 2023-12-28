28c6bdb66252904c80464ef9e0a0a72f8e1a7839
Michael Lake <mlake@alleywayapps.com>
2023-12-28 14:59:13 +0100

    try with backupCode

--- dist/add-alerts.test.js
+++ dist/add-alerts.test.js
@@ -24,6 +24,7 @@ ASDFTEST,ASDF,TEST, "creating invalid symbol"
         config.tradingview.chartUrl = process.env.TEST_TRADINGVIEW_CHART;
         config.tradingview.username = process.env.TEST_TRADINGVIEW_USERNAME;
         config.tradingview.password = process.env.TEST_TRADINGVIEW_PASSWORD;
+        config.tradingview.backupCode = process.env.TEST_TRADINGVIEW_BACKUP_CODE;
         config.alert.actions.webhook.enabled = false;
         //config.tradingview.interval = "1h, 4h"
         // write it out again
